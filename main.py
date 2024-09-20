41bd482108794abc78405641dbbc2b541d496b87
Henry Kyle Frese <h.kyle.frese@gmail.com>
2024-09-20 13:13:39 -0700

    Add greet person and updated test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
-def hello_world():
-    return "Hello, from Kyle Frese!"
+def greet_person(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    print(hello_world())
+    print(greet_person())
--- test_main.py
+++ test_main.py
@@ -1,10 +1,11 @@
 # test_main.py
 import unittest
-from main import hello_world
+from main import greet_person
 
-class TestHelloWorld(unittest.TestCase):
-    def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, from Kyle Frese!")
+class TestMain(unittest.TestCase):
+    def test_greet_person(self):
+        self.assertEqual(greet_person("Kyle"), "Hello, Kyle!")
+        self.assertEqual(greet_person("Morgan"), "Hello, Morgan!")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
